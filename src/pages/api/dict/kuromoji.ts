11a7011a8bda88791d9f300691c5b0deb27afb7c
ishiko732 <ishiko732@gmail.com>
2023-07-06 18:07:46 +0800

    Doc/kuromoji LICENSE

--- src/pages/api/dict/kuromoji.ts
+++ src/pages/api/dict/kuromoji.ts
@@ -1,6 +1,12 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import tokenizer from "@/service/kuromoji";
 
+/**
+ * github : https://github.com/takuyaa/kuromoji.js
+ * NPM : https://www.npmjs.com/package/kuromoji
+ * LICENSE : Apache-2.0
+ */
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
     const text = req.query.text as string | null;
     if (typeof text === "string") {
