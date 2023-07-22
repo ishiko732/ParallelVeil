845cfe112972c825c4b1a580ae9d103f688323d9
ishiko732 <ishiko732@gmail.com>
2023-07-22 17:59:13 +0800

    Feat/article split to words

--- src/service/analyzer/en_US.ts
+++ src/service/analyzer/en_US.ts
@@ -2,7 +2,7 @@ export default function splitWords(str: string): string[] {
     var strings = str.split(/\s+|(?=[".,:;!?()])/).filter(text => text !== '');
     var res: string[] = []
     for (let i = 0; i < strings.length; i++) {
-        res.push(...strings[i].split(/(?<=["(])/))
+        res.push(...strings[i].split(/(?<=["(.])/))
     }
     return res
 }
