2ad5555bc489d86116214bb191c8783805c0ac77
ishiko732 <ishiko732@gmail.com>
2023-08-27 01:49:49 +0800

    Feat/home redirect to article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Counter from "@/components/counter";
-import {Fragment} from "react";
 import {Metadata} from "next";
 import process from "process";
-import {loggerDebug} from "@/config/pinoConfig";
+import {redirect} from 'next/navigation';
 
 export const metadata: Metadata = {
     title: `Parallel Veil - ${process.env.uid}`
 }
 
 export default function Page() {
-    return <Fragment>
-        <Counter/>
-    </Fragment>
+    redirect('/article');
 }
