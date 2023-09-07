052c52d8f3cd63e2e836765baf9ce74e1f4d2350
ishiko732 <ishiko732@gmail.com>
2023-09-07 20:40:26 +0800

    Fix/iphone show image layout wrong

--- src/app/(article)/article/_components/unified/PVImage.tsx
+++ src/app/(article)/article/_components/unified/PVImage.tsx
@@ -4,7 +4,7 @@ import PVSpan from "@/app/(article)/article/_components/unified/PVSpan";
 export default function PVImage({ src, alt, style, className, children }: any) {
   const spans = splitWords(alt);
   return (
-    <div className={"sm:flex justify-center items-center flex-col"}>
+    <div className={"flex justify-center items-center flex-col"}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img alt={alt} src={src} style={style} className={className}>
         {children}
